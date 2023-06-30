4514a366b480547692613790a3ce4aaf5dc0db66
Dafna Nicole <111672427+thvx@users.noreply.github.com>
2023-06-30 14:27:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,62 @@
-from autenticacion import AutenticadorGmail
-from sincronizacion import SincronizarGmail
-from moverSpam import moverSpam
+from login import Login
+from configuracionUsuario import ConfiguracionUsuario
 
-# Configuración de la API de Gmail
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-CREDENTIALS_FILE = 'Ingeniería de Requisitos/Bote de spam/credentials.json'
+scopes = ['https://www.googleapis.com/auth/gmail.readonly']
+archivoCredenciales = 'credentials.json'
 
-def menu():
+def menuPrincipal():
     print("Seleccione una opción:")
-    print("1. Sincronizar correos electrónicos no leídos")
-    print("2. Mover correos electrónicos marcados como spam")
+    print("1. Registrar usuario")
+    print("2. Iniciar sesión")
     print("3. Salir")
 
-while True:
-    menu()
-    opcion = input("Opción: ")
+def menuUsuario():
+    print("Seleccione una opción:")
+    print("1. Configuracion de usuario")
+    print("2. Sincronizar correo")
+    print("3. Detectar Spam")
+    print("4. Volver al menú principal")
 
-    if opcion == "1":
-        authenticator = AutenticadorGmail(CREDENTIALS_FILE, SCOPES)
-        creds = authenticator.autenticar()
-        synchronizer = SincronizarGmail(creds)
-        synchronizer.sincronizarEmailNoLeido()
-    elif opcion == "2":
-        authenticator = AutenticadorGmail(CREDENTIALS_FILE, SCOPES)
-        creds = authenticator.autenticar()
-        mover = moverSpam(creds)
-        mover.obtenerCarpetasCorreo()
-        spam_folder_name = input("Ingrese el nombre de la carpeta de spam: ")
-        mover.moverCorreo(spam_folder_name)
-    elif opcion == "3":
-        break
-    else:
-        print("Opción inválida. Intente nuevamente.")
+def ejecutar():
+    login = Login(archivoCredenciales, scopes)
+
+    opcionPrincipal = 0
+    opcionUsuario = 0
+
+    while opcionPrincipal != 3:
+        menuPrincipal()
+        opcionPrincipal = int(input("Opcion: "))
+
+        if opcionPrincipal == 1:
+            direccionCorreo = input("Ingrese su direccion de correo: ")
+            login.registrarUsuario(direccionCorreo)
+        elif opcionPrincipal == 2:
+            direccionCorreo = input("Ingrese su direccion de correo: ")
+            if login.iniciarSesion(direccionCorreo):
+                while opcionUsuario != 4:
+                    menuUsuario()
+                    opcionUsuario = int(input("Opcion: "))
+                    if opcionUsuario == 1:
+                        ConfiguracionUsuario.menu()
+                        opcionConf = int(input("Opcion: "))
+                        if opcionConf == 1:
+                            ConfiguracionUsuario.modificarDatos(direccionCorreo)
+                        elif opcionConf == 2:
+                            ConfiguracionUsuario.eliminarCuenta(direccionCorreo)
+                        else:
+                            break
+                    elif opcionUsuario == 2:
+                        print("Sincronizar correo")
+                    elif opcionUsuario == 3:
+                        print("Detectar Spam")
+                    elif opcionUsuario == 4:
+                        break
+                    else:
+                        print("Operacion invalida. Intente nuevamente.")
+            elif opcionPrincipal == 3:
+                break
+        else:
+            print("Opcion invalida. Intente nuevamente.")
+
+if __name__ == "__main__":
+    ejecutar()
